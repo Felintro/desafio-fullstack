e1c9eb5487e027d41663c45880e297ededeb6e25
Allan <allanpdu@gmail.com>
2024-04-19 18:55:53 -0300

    Implementação de métodos para realizar o de-para de listas de objetos em listas de DTO

--- backend/desafio-fullstack/src/main/java/br/com/gazin/desafiofullstack/utils/DeveloperMapper.java
+++ backend/desafio-fullstack/src/main/java/br/com/gazin/desafiofullstack/utils/DeveloperMapper.java
@@ -5,6 +5,9 @@ import br.com.gazin.desafiofullstack.dto.LevelDTO;
 import br.com.gazin.desafiofullstack.model.Developer;
 import br.com.gazin.desafiofullstack.model.Level;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DeveloperMapper {
 
     static Developer toEntity(DeveloperDTO developerDTO) {
@@ -15,6 +18,12 @@ public class DeveloperMapper {
     static DeveloperDTO toDTO(Developer developer) {
         LevelDTO levelDTO = LevelMapper.toDTO(developer.getLevel());
         return new DeveloperDTO(developer.getId(), levelDTO, developer.getName(), developer.getBirthdate(), developer.getAge(), developer.getHobby());
+    }
+
+    public static List<DeveloperDTO> toDTOList(List<Developer> developerList) {
+        List<DeveloperDTO> dtoList = new ArrayList<>();
+        developerList.forEach(developer -> dtoList.add(toDTO(developer)));
+        return dtoList;
     }
 
 }
--- backend/desafio-fullstack/src/main/java/br/com/gazin/desafiofullstack/utils/LevelMapper.java
+++ backend/desafio-fullstack/src/main/java/br/com/gazin/desafiofullstack/utils/LevelMapper.java
@@ -3,6 +3,9 @@ package br.com.gazin.desafiofullstack.utils;
 import br.com.gazin.desafiofullstack.dto.LevelDTO;
 import br.com.gazin.desafiofullstack.model.Level;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LevelMapper {
 
     static Level toEntity(LevelDTO levelDTO) {
@@ -11,6 +14,12 @@ public class LevelMapper {
 
     static LevelDTO toDTO(Level level) {
         return new LevelDTO(level.getId(), level.getLevel());
+    }
+
+    public static List<LevelDTO> toDTOList(List<Level> levelList) {
+        List<LevelDTO> dtoList = new ArrayList<>();
+        levelList.forEach(level -> dtoList.add(toDTO(level)));
+        return dtoList;
     }
 
 }
