a3d36fafc468e9829254692764dc3a2e80c4e751
Allan <allanpdu@gmail.com>
2024-04-19 20:41:43 -0300

    Mudança para o idioma português, atendendo os nomes solicitados no escopo do desafio.

--- backend/desafio-fullstack/src/main/java/br/com/gazin/desafiofullstack/dto/DeveloperDTO.java
+++
@@ -1,7 +0,0 @@
-package br.com.gazin.desafiofullstack.dto;
-
-import java.time.LocalDate;
-
-public record DeveloperDTO(Integer id, LevelDTO levelDTO, String name, LocalDate birthdate, Integer age, String hobby)  {
-
-}
