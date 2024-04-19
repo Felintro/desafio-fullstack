ed8964e0858491e1aa4184c8797c5773eb3efa83
Allan <allanpdu@gmail.com>
2024-04-18 21:59:20 -0300

    Descrição semântica dos repositories

--- backend/desafio-fullstack/src/main/java/br/com/gazin/desafiofullstack/persistence/DeveloperRepository.java
+++ backend/desafio-fullstack/src/main/java/br/com/gazin/desafiofullstack/persistence/DeveloperRepository.java
@@ -2,7 +2,9 @@ package br.com.gazin.desafiofullstack.persistence;
 
 import br.com.gazin.desafiofullstack.model.Developer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface DeveloperRepository extends JpaRepository<Developer, Integer> {
 
 }
--- backend/desafio-fullstack/src/main/java/br/com/gazin/desafiofullstack/persistence/LevelRepository.java
+++ backend/desafio-fullstack/src/main/java/br/com/gazin/desafiofullstack/persistence/LevelRepository.java
@@ -2,7 +2,9 @@ package br.com.gazin.desafiofullstack.persistence;
 
 import br.com.gazin.desafiofullstack.model.Level;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface LevelRepository extends JpaRepository<Level, Integer> {
 
 }
