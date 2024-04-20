856ac63cbf6808668e56a655e568bb287883b3b5
Allan <allanpdu@gmail.com>
2024-04-20 12:20:05 -0300

    Implementação dos métodos GET, POST e DELETE

--- backend/desafio-fullstack/src/main/java/br/com/gazin/desafiofullstack/controller/NivelController.java
+++ backend/desafio-fullstack/src/main/java/br/com/gazin/desafiofullstack/controller/NivelController.java
@@ -2,9 +2,17 @@ package br.com.gazin.desafiofullstack.controller;
 
 import br.com.gazin.desafiofullstack.dto.NivelDTO;
 import br.com.gazin.desafiofullstack.service.NivelService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.persistence.PersistenceException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,7 +22,7 @@ import java.util.List;
 @RequestMapping("/api/niveis")
 public class NivelController {
 
-    private NivelService nivelService;
+    private final NivelService nivelService;
 
     public NivelController(NivelService nivelService) {
         this.nivelService = nivelService;
@@ -22,8 +30,32 @@ public class NivelController {
 
     @GetMapping
     public ResponseEntity<List<NivelDTO>> retornarTodos() {
-        List<NivelDTO> dtoListResponse = nivelService.buscarTodos();
-        return ResponseEntity.status(HttpStatus.OK).body(dtoListResponse);
+        var dtoListResponse = nivelService.buscarTodos();
+        if(dtoListResponse.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(dtoListResponse);
+        } else {
+            return ResponseEntity.status(HttpStatus.OK).body(dtoListResponse);
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<NivelDTO> cadastrar(@RequestBody String json) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode jsonNode = objectMapper.readTree(json);
+        String nivel = jsonNode.get("nivel").asText();
+
+        var nivelDTO = nivelService.cadastrar(nivel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(nivelDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity deletarPorId(@PathVariable Integer id) {
+        try {
+            nivelService.deletarPorId(id);
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).body("Nível removido com sucesso!");
+        } catch(PersistenceException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Existem desenvolvedores associados a este nível!");
+        }
     }
 
 }
--- backend/desafio-fullstack/src/main/java/br/com/gazin/desafiofullstack/service/NivelService.java
+++ backend/desafio-fullstack/src/main/java/br/com/gazin/desafiofullstack/service/NivelService.java
@@ -4,6 +4,7 @@ import br.com.gazin.desafiofullstack.dto.NivelDTO;
 import br.com.gazin.desafiofullstack.model.Nivel;
 import br.com.gazin.desafiofullstack.persistence.NivelRepository;
 import br.com.gazin.desafiofullstack.utils.NivelMapper;
+import jakarta.persistence.PersistenceException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -18,9 +19,19 @@ public class NivelService {
     }
 
     public List<NivelDTO> buscarTodos() {
-        List<Nivel> nivelList = nivelRepository.findAll();
-        List<NivelDTO> dtoList = NivelMapper.toDTOList(nivelList);
+        var nivelList = nivelRepository.findAll();
+        var dtoList = NivelMapper.toDTOList(nivelList);
         return dtoList;
     }
 
+    public void deletarPorId(Integer id) {
+        var nivel = nivelRepository.findById(id).orElseThrow(PersistenceException::new);
+        nivelRepository.delete(nivel);
+    }
+
+    public NivelDTO cadastrar(String nomeNivel) {
+        var nivel = new Nivel(null, nomeNivel);
+        nivelRepository.save(nivel);
+        return NivelMapper.toDTO(nivel);
+    }
 }
