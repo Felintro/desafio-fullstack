41442f749129822ec9b935b5685d0f587985f706
Allan <allanpdu@gmail.com>
2024-04-21 17:57:18 -0300

    Melhora no tratamento de erros

--- backend/desafio-fullstack/src/main/java/br/com/gazin/desafiofullstack/service/NivelService.java
+++ backend/desafio-fullstack/src/main/java/br/com/gazin/desafiofullstack/service/NivelService.java
@@ -1,16 +1,17 @@
 package br.com.gazin.desafiofullstack.service;
 
 import br.com.gazin.desafiofullstack.dto.NivelDTO;
-import br.com.gazin.desafiofullstack.model.Nivel;
 import br.com.gazin.desafiofullstack.persistence.NivelRepository;
 import br.com.gazin.desafiofullstack.utils.NivelMapper;
-import jakarta.persistence.PersistenceException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class NivelService {
+
+    private final String NIVEL_NAO_ENCONTRADO = "O nível não foi encontrado! ID: ";
 
     private NivelRepository nivelRepository;
 
@@ -31,14 +32,14 @@ public class NivelService {
     }
 
     public NivelDTO atualizar(Integer id, NivelDTO nivelDTO) {
-        var nivel = nivelRepository.findById(id).orElseThrow(PersistenceException::new);
+        var nivel = nivelRepository.findById(id).orElseThrow(() -> new EntityNotFoundException(NIVEL_NAO_ENCONTRADO + id));
         nivel.setNivel(nivelDTO.nivel());
         nivelRepository.save(nivel);
         return NivelMapper.toDTO(nivel);
     }
 
     public void deletarPorId(Integer id) {
-        var nivel = nivelRepository.findById(id).orElseThrow(PersistenceException::new);
+        var nivel = nivelRepository.findById(id).orElseThrow(() -> new EntityNotFoundException(NIVEL_NAO_ENCONTRADO + id));
         nivelRepository.delete(nivel);
     }
 
