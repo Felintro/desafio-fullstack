e31d452de713e7213ca06153c2699e4700ee0d88
Allan <allanpdu@gmail.com>
2024-04-22 20:20:01 -0300

    criação de estrutura inicial para desenvolvedor e nivel

---
+++ frontend/desafio-fullstack/src/app/view/desenvolvedor/desenvolvedor.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-desenvolvedor',
+  templateUrl: './desenvolvedor.component.html',
+  styleUrl: './desenvolvedor.component.scss'
+})
+export class DesenvolvedorComponent {
+
+}
