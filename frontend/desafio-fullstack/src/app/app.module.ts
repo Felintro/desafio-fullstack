777eb117e1196750a7c80dbe416e9c2bab6396e0
Allan <allanpdu@gmail.com>
2024-04-22 20:44:06 -0300

    Inclusão dos botões de ações na tabela e import das suas dependências

--- frontend/desafio-fullstack/src/app/app.module.ts
+++ frontend/desafio-fullstack/src/app/app.module.ts
@@ -3,27 +3,33 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { HomeComponent } from './view/home/home.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import {MatTableModule} from '@angular/material/table';
-
-
+import { NivelComponent } from './view/nivel/nivel.component';
+import { DesenvolvedorComponent } from './view/desenvolvedor/desenvolvedor.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    NivelComponent,
+    DesenvolvedorComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MatToolbarModule,
-    MatTableModule
+    MatTableModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [
     provideClientHydration(),
